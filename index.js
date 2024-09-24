db4b92ba8062bd6dca429aba8db9e1d79befe074
Anushka-engg <anushkasarkar1176@gmail.com>
2024-09-24 18:15:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+//Rules button
+function Show(id) {
+    if (document.getElementById(id).style.display == 'none') {
+        document.getElementById(id).style.display = 'block';
+    } else {
+        document.getElementById(id).style.display = 'none';
+    }
+};
+
+//Submit Button
+function Verify() {
+    var userRef = < input ref = { myRef }
+    value = { myRef.current.value }
+    />
+    var passRef = < input ref = { myRef }
+    value = { myRef.current.value }
+    />
+}
